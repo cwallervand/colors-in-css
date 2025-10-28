f6b715aa0b6c7a653ff49668253fd5f38384b89e
Christian Wallervand <cwallervand@gmail.com>
2025-10-28 10:47:44 +0100

    Added resources page (not used yet)

---
+++ src/pages/resources/resources.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'resources',
+  standalone: true,
+  imports: [],
+  templateUrl: './resources.html',
+})
+export class Resources {}
