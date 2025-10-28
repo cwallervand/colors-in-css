880b4495ed7bc57cc22f3904d440b1af841c157e
Christian Wallervand <cwallervand@gmail.com>
2025-10-28 11:57:07 +0100

    Changed route paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,25 @@
 import { Routes } from '@angular/router';
 
 import { BasicColorPage } from '../pages/basic-color/basic-color';
+import { BasicRelativeColorsPage } from '../pages/basic-relative-colors/basic-relative-colors';
+import { ResourcesPage } from '../pages/resources/resources';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: '1',
     component: BasicColorPage,
+  },
+  {
+    path: '2',
+    component: BasicRelativeColorsPage,
+  },
+  {
+    path: 'resources',
+    component: ResourcesPage,
+  },
+  {
+    path: '',
+    redirectTo: '1',
+    pathMatch: 'full',
   },
 ];
