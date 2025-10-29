472b0ad2cd7ee3433b7a9151fb1a6b8297498459
Christian Wallervand <cwallervand@gmail.com>
2025-10-29 13:59:17 +0100

    Styling to header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 // import { BasicColorPage } from '../pages/basic-color/basic-color';
 // import { BasicRelativeColorsPage } from '../pages/basic-relative-colors/basic-relative-colors';
 import { ResourcesPage } from '../pages/resources/resources';
-import { PageOrchestrator } from './page-orchestrator';
+import { PageOrchestrator } from './page-orchestrator/page-orchestrator';
 
 export const routes: Routes = [
   {
