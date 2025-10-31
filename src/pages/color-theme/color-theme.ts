7fe62ce42f4528b4e60d1cb3193cbe77a8d9cab9
Christian Wallervand <cwallervand@gmail.com>
2025-10-31 08:14:53 +0100

    Starte work on color theme page

---
+++ src/pages/color-theme/color-theme.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  templateUrl: './color-theme.html',
+  styleUrl: './color-theme.css',
+})
+export class ColorThemePage {}
